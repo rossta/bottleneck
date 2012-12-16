78b5599d1be2fdb7ca4a7795f1398ac909a8decc
Ross Kaffenberger <rosskaff@gmail.com>
2012-12-16 14:45:25 -0500

    redis initializer

---
+++ config/initializers/redis.rb
@@ -0,0 +1,4 @@
+require 'redis'
+require 'redis/objects'
+redis_config = YAML.load_file('config/redis.yml')[Rails.env]
+Redis.current = Redis.new(host: redis_config['host'], port: redis_config['port'])
