7803c25441d9b609dea8653324a7cbad9a07837d
Ross Kaffenberger <rosskaff@gmail.com>
2013-01-07 09:32:01 -0500

    don't initialize env on precompile

--- config/application.rb
+++ config/application.rb
@@ -72,5 +72,8 @@ module Bottleneck
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Prevent environment initialization on precompile
+    config.assets.initialize_on_precompile = false
   end
 end
