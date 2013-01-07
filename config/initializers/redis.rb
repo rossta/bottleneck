bc8b57520adae676c6c227a31c7a17a8236dcace
Ross Kaffenberger <rosskaff@gmail.com>
2013-01-07 10:10:54 -0500

    missing password from redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,4 +3,4 @@ require 'redis/objects'
 url = ENV['REDISTOGO_URL']
 raise "REDISTOGO_URL not defined!!!" unless url
 uri = URI.parse(url)
-Redis.current = Redis.new(host: uri.host, port: uri.port)
+Redis.current = Redis.new(host: uri.host, port: uri.port, passwor: uri.password)
