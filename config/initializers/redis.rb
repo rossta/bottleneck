0443d835956448bb59e2431c45816527abe70d55
Ross Kaffenberger <rosskaff@gmail.com>
2013-01-07 09:29:41 -0500

    update redis initialier

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 require 'redis'
 require 'redis/objects'
 url = ENV['REDISTOGO_URL']
+raise "REDISTOGO_URL not defined!!!" unless url
 uri = URI.parse(url)
 Redis.current = Redis.new(host: uri.host, port: uri.port)
