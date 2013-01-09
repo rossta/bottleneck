46b8fe8b10b9c68920c86be7e9ae3044f5a36637
Ross Kaffenberger <rosskaff@gmail.com>
2013-01-09 09:46:57 -0500

    Specs for trello account

--- spec/factories/trello_accounts.rb
+++ spec/factories/trello_accounts.rb
@@ -1,10 +1,8 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  sequence(:account_name) { |n| "trello_account_#{n}" }
-
   factory :trello_account do
-    name { generate(:account_name) }
+    name 'bottleneck'
     token "aeb20eb3201ad981ffc1a65ee6f3d9225de5491e729835e22ecdb3a5551ac5f9"
   end
 end
--- spec/models/trello_account_spec.rb
+++ spec/models/trello_account_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe TrelloAccount do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#trello_member", vcr: { record: :new_episodes } do
+    let(:trello_account) { build(:trello_account) }
+
+    it "fetches member data for given trello account name" do
+      member = trello_account.trello_member
+      member.username.should eq(trello_account.name)
+    end
+  end
+
+  describe "#trello_boards", vcr: { record: :new_episodes } do
+    let(:trello_account) { build(:trello_account) }
+
+    it "fetches trello board data" do
+      boards = trello_account.trello_boards
+      names = boards.map(&:name)
+      names.should include('Bottleneck')
+      names.should include('Welcome Board')
+    end
+  end
 end
