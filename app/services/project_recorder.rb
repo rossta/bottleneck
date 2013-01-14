f2605284254698d952ab6d885470ec4a4acddb31
Ross Kaffenberger <rosskaff@gmail.com>
2013-01-14 11:20:14 -0500

    run project recorder in foreground

--- app/services/project_recorder.rb
+++ app/services/project_recorder.rb
@@ -2,14 +2,17 @@ class ProjectRecorder
   @queue = :account
 
   def self.record_interval
-    Project.select("id").find_each do |project|
-      Resque.enqueue(self, project.id)
+    Project.find_each do |project|
+      record_project project
     end
   end
 
   def self.perform(project_id)
     puts "Fetching project #{project_id} and recording interval"
-    project = Project.find(project_id)
+    record_project Project.find(project_id)
+  end
+
+  def self.record_project(project)
     project.fetch
     project.record_interval
   end
